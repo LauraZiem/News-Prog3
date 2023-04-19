73837dd2e82c4bfd314b2ac445bcdb6634820409
Laura Ziem Teles França <375278@unoesc.lan>
2023-04-18 22:02:48 -0300

    primeiro commit

---
+++ News/News/Bootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using News.ViewModels;
+
+namespace News
+{
+    public static class Bootstrapper
+    {
+        public static void Initialize()
+        {
+            var containerBuider = new ContainerBuilder();
+
+            containerBuider.RegisterType<MainShell>();
+
+            containerBuider.RegisterAssemblyTypes(typeof(App).Assembly)
+                .Where(x => x.IsSubclassOf(typeof(ViewModel)));
+            var container = containerBuider.Build();
+            Resolver.Initialize(container);
+        }
+    }
+}
